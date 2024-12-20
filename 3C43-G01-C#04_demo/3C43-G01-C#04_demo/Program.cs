5ca8be59cb49e3ca6dfd24b819510c654b19a32f
omar02salem <omarsalem310@gmail.com>
2024-12-21 01:38:48 +0200

    session 4

---
+++ 3C43-G01-C#04_demo/3C43-G01-C#04_demo/Program.cs
@@ -0,0 +1,366 @@
+﻿using System.ComponentModel;
+using System.Text;
+using System.Threading.Channels;
+
+namespace _3C43_G01_C_04_demo
+{
+    internal class Program
+    {
+        #region FunctionsParams
+
+       /* static int SumArray(params int[] Arr)
+        {
+            int Sum = 0;
+            Arr[0] = 0;
+            if(Arr is not null )
+            {
+                for (int i = 0; i < Arr.Length; i++)
+                {
+                    Sum += Arr[i];
+                }
+            }
+            return Sum;
+        }*/
+
+        #endregion
+        #region passing by out
+        /*public static void SumMul(int X ,int Y, out int Sum,out int Mul)
+        {
+            Sum = X + Y ;
+            Mul = X * Y ;
+            
+        }*/
+        #endregion
+        #region passing by ref and value
+        /*public static void Swap(ref int X, ref int Y)
+        {
+            int Temp = X;
+            X = Y;
+            Y = Temp;
+        }*/
+        /*public static void Print(string Pattern,int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write(Pattern);
+            }
+            Console.WriteLine();
+        }
+        public static void PrintPattern(int count, string Pattern = "#")
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write(Pattern);
+            }
+            Console.WriteLine();
+        }*/
+        #endregion
+        static void Main(string[] args)
+        {
+            #region String
+
+            //string is a Class [Reference Type ] , Internally is an Array of Chars
+
+            /*string Name;
+            //Declare for Reference of type "String"
+            //this reference 'name' id Refering to the default value of Reference Type =Null
+            //CLR Will allocate 4 Bytes at STACK for the Reference 'Name'
+            //CLR will allocate 0 Bytes at HEAP 
+
+            Name = new string("omar"); //CLR will Allocate 8 Bytes at HEAP
+
+            Name = "omar";
+            Console.WriteLine(Name);
+            Console.WriteLine($"hash code is {Name.GetHashCode()}");*/
+
+            /* string Name01 = "Ahmed "; //has 2 references 
+             string Name02 = "Omar";*/ // unreachable object
+            /*Console.WriteLine(Name01);
+            Console.WriteLine(Name02);
+            Console.WriteLine(Name01.GetHashCode());
+            Console.WriteLine(Name02.GetHashCode());*/
+
+            /*Name02 = Name01;// Console.WriteLine("after Name02 = Name01");
+
+            Name02 = "Yassmin";
+
+            Console.WriteLine(Name01);
+            Console.WriteLine(Name02);*/
+
+            /*Console.WriteLine(Name01);
+            Console.WriteLine(Name02);
+            Console.WriteLine(Name01.GetHashCode());
+            Console.WriteLine(Name02.GetHashCode());*/
+
+
+            /*string Message = "Hello";
+            Console.WriteLine($"Message : {Message}");
+            Console.WriteLine(Message.GetHashCode());
+
+            Message += "omar";//initiated new object
+            Console.WriteLine("After changing the message");
+
+            Console.WriteLine(Message.GetHashCode());*/
+
+            #endregion
+
+            #region StringBuilder
+
+            //string builder is a Class [Reference Type] and is Mutable strung [internaly is a linked list of Chars]
+
+            /*StringBuilder Message ;
+            //Declare a Referece of type 'ReferencBuilder'
+            //CLR will Allocate 4 Bytes at the STACK for the Reference 'Message'
+            //CLR will Allocate 0 Bytes at the Heap
+
+            Message = new StringBuilder("Hello");
+            Console.WriteLine($"Message is {Message}");
+            Console.WriteLine($"Message Hashcode is {Message.GetHashCode()}");
+
+            Message.Append(" Omar");
+            Message.AppendLine(" salem  ");
+            Message.AppendLine(" you age is 22 ");
+            Message.Remove(0, 5);
+            Message.Insert(0,"hi");
+            Message.AppendFormat(" you{0} age is{1} 22 ", "omar",true);
+            Message.AppendJoin(";", "omar", "mohamed", "salem");
+            Console.WriteLine($"Message is {Message}");
+            Console.WriteLine($"Message is {Message.GetHashCode()}");*/
+
+            #endregion
+
+            #region Arrays -one dimension
+
+            //array is a Class : Reference Type
+            /*int[] Numbers;
+            //References to Null
+
+            Numbers = new int[3*//*don't have to*//*] { 1,2,3};
+            Numbers = [5,6,7];
+            //CLR will allocate 12 bytes at the heap will be intialized by 0 
+            *//*Numbers[0] = 0;
+            Numbers[1] = 1;
+            Numbers[2] = 2;*//*
+
+            Console.WriteLine(Numbers[0]);          
+            Console.WriteLine(Numbers[1]);
+            Console.WriteLine(Numbers[2]);
+            Console.WriteLine($"the number of number is {Numbers.Length} The diminsint is {Numbers.Rank}");
+
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                Console.WriteLine(Numbers[i]);
+            }*/
+            //arays o(1) to select an item 
+            //arrays have fixed length
+            #endregion
+
+            #region Arrays - Two D 
+            /*int[,] Marks = new int[3, 5];
+            Console.WriteLine(Marks.Rank);
+
+            Console.Clear();
+            Console.Beep(2000,1500);
+
+            bool flag;
+            for (int i = 0; i < Marks.GetLength(0); i++)
+            {
+                Console.WriteLine($"please enter the markse of the student no{i + 1}");
+                //do while is better here
+                for (int j = 0; j <Marks.GetLength(1); *//*j++*//*)
+                {
+                    Console.WriteLine($"please enter the mark of the subject no{j+1}");
+                    flag=int.TryParse(Console.ReadLine(), out Marks[i, j]);
+                    if (flag)
+                        j++;
+                }
+            }
+
+            Console.Clear();
+
+            for (int i = 0; i < Marks.GetLength(0); i++)
+            {
+                Console.WriteLine($"the data for student no {i+1} is");
+                for (int j = 0; j < Marks.GetLength(1);j++)
+                {
+
+                    Console.WriteLine($"the grade of subject no {j + 1} is {Marks[i, j]}");
+                }
+                Console.WriteLine("======================");
+            }*/
+
+            /* Marks[0, 0] = 1;
+             Marks[0, 1] = 2;
+             Marks[0, 2] = 3;
+             Marks[0, 3] = 4;
+             Marks[0, 4] = 5;
+
+             Marks[1, 0] = 1;
+             Marks[1, 1] = 2;
+             Marks[1, 2] = 3;
+             Marks[1, 3] = 4;
+             Marks[1, 4] = 5;
+
+             Marks[2, 0] = 1;
+             Marks[2, 1] = 2;
+             Marks[2, 2] = 3;
+             Marks[2, 3] = 4;
+             Marks[2, 4] = 5;
+
+             Marks[3, 0] = 1;
+             Marks[3, 1] = 2;
+             Marks[3, 2] = 3;
+             Marks[3, 3] = 4;
+             Marks[3, 4] = 5;*/
+
+
+            /*int[,] Numbers = { {1,2,3 },{4,5,6 } };
+            for (int i = 0; i < Numbers.GetLength(0) * Numbers.GetLength(1); i++)
+            {
+                Console.WriteLine(Numbers[i / Numbers.GetLength(1), i % Numbers.GetLength(1)]);
+            }*/
+
+            #endregion
+
+            #region Two D array judged
+
+            /*int[][] Marks = new int[3][];
+            Marks[0] = new int[3] { 9, 8, 3 };
+            Marks[1] = new int[2] { 7, 2 };
+            Marks[2] = new int[1] { 5 };*/
+
+
+            #endregion
+
+            #region Array Methods
+
+            //1- Class member methods
+
+            // the Class named Array [static]
+
+            /*int[] numbers = { 1, 2, 6, 5, 2, 8, 4, 8, 9, 0, 4, 97 };
+
+
+            Array.Sort(numbers);
+
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            int[] array1 = { 1, 2, 3 };
+            int[] array2 = new int[2];
+
+
+            Array.Copy(array1, array2, 2);
+            foreach (int number in array2)
+            {
+            Console.WriteLine(number); 
+            }
+            Array.Clear(array1);
+            //Array.ConstrainedCopy();
+            
+
+            Array array3 = Array.CreateInstance(typeof(char), 10);
+
+            int[] array4 = (int[])Array.CreateInstance(typeof(int),5,4);
+            */
+
+            //2- object member methods [non-static]
+
+            /*int[] array = { 1, 2 };
+            Array array2 = new int[4];
+            array.CopyTo(array2,2);
+            foreach(int i in array2)
+                Console.WriteLine(i.GetHashCode());
+
+            array.GetLength(0);
+            int a = 0;  */
+
+
+            #endregion
+
+            #region Functions
+
+            //functions has two components
+            //1- prototype signature RetrunType and the name paraamters and the body
+            //2-Calling of Function
+            /* Program.Print("#",5);
+             PrintPattern(5,"$");
+
+             //to not pass arguments by different order you should pass by name
+             PrintPattern(  10, Pattern: "^");*/
+            #region Passing By Value and reference
+            //passing by reference 
+            /* int A = 5, B =8;
+
+             Swap(ref A,ref B);
+
+             Console.WriteLine($"After The Swap is A :{A} and B: {B}");*/
+
+
+
+            #endregion
+
+            //passing by out
+
+            #region passing by out
+            /*int A = 10, B = 5;
+                SumMul(A, B, out int sumres, out int multres);
+                Console.WriteLine($"sum :{sumres} multi :{multres}");*/
+            #endregion
+
+            #region Functions-Params
+
+            /*int[] number = { 1, 2, 3, 4, 5, 6, 7 };
+            int totatl= SumArray(number);
+            Console.WriteLine(totatl);
+            Console.WriteLine(number[0]);*/
+
+            #endregion
+
+            #endregion
+
+            #region Boxing vs UnBoxing
+
+            //Boxing : casting from value type to reference type 
+            //UnBoxing : casting from reference type to value type
+            #region ex1
+            /*object obj;
+            //declare for reference of type object refer to null
+            //the reference can refere to instance of Object 
+            obj = new object();
+            obj = new string("omar ");
+            obj = 'O';//Casting from char [Value Type ] to object [Refernce Type ] implicitly
+
+            obj = 3;//Casting from inter [Value Type ] to object [Refernce Type ] implicitly
+
+            obj = true; //Casting from boolian [Value Type ] to object [Refernce Type ] implicitly
+
+            bool flag = (bool)obj;//unboxing 
+            Console.WriteLine(flag);*/
+            #endregion
+
+            #region ex2
+            /* //Boxing 
+             int X = 5;
+
+             object O1 = X;
+             //casting from int to object  Boxing safe casting impilicit castin 
+
+             Console.WriteLine(O1);
+
+             //UnBoxing 
+             object O2 = 10;
+
+             O2 = "ahmed";
+
+             int Y = (int)O2;*/
+
+            #endregion
+            #endregion
+
+            int x;
+            Console.WriteLine(x);
+        }
+    }
+}
---
+++ C43-G03-C#04_assignment/C43-G03-C#04_assignment/Program.cs
@@ -0,0 +1,293 @@
+﻿namespace C43_G03_C_04_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Points on a Single Line
+            Console.WriteLine("Enter coordinates for three points:");
+            Console.Write("x1: ");
+            double x1 = double.Parse(Console.ReadLine());
+            Console.Write("y1: ");
+            double y1 = double.Parse(Console.ReadLine());
+
+            Console.Write("x2: ");
+            double x2 = double.Parse(Console.ReadLine());
+            Console.Write("y2: ");
+            double y2 = double.Parse(Console.ReadLine());
+
+            Console.Write("x3: ");
+            double x3 = double.Parse(Console.ReadLine());
+            Console.Write("y3: ");
+            double y3 = double.Parse(Console.ReadLine());
+
+            double slope1 = (y2 - y1) / (x2 - x1);
+            double slope2 = (y3 - y2) / (x3 - x2);
+
+            if (slope1 == slope2)
+            {
+                Console.WriteLine("The points lie on a single straight line.");
+            }
+            else
+            {
+                Console.WriteLine("The points do not lie on a single straight line.");
+            }
+            #endregion
+
+            #region Worker Efficiency Evaluation
+            Console.Write("Enter time taken to complete the task (in hours): ");
+            double time = double.Parse(Console.ReadLine());
+
+            if (time >= 2 && time < 3)
+            {
+                Console.WriteLine("Highly efficient.");
+            }
+            else if (time >= 3 && time < 4)
+            {
+                Console.WriteLine("Increase speed.");
+            }
+            else if (time >= 4 && time < 5)
+            {
+                Console.WriteLine("Training to enhance speed.");
+            }
+            else if (time >= 5)
+            {
+                Console.WriteLine("Leave the company.");
+            }
+            #endregion
+
+            #region Identity Matrix
+            Console.Write("Enter the size of the identity matrix (n): ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == j)
+                    {
+                        Console.Write("1 ");
+                    }
+                    else
+                    {
+                        Console.Write("0 ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            #endregion
+
+            #region Sum of All Elements of the Array
+            Console.WriteLine("Enter the elements of the array (space-separated):");
+            int[] array = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            int sum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+
+            Console.WriteLine($"Sum of all elements: {sum}");
+            #endregion
+
+            #region Merge Two Sorted Arrays
+            Console.WriteLine("Enter the elements of the first sorted array (space-separated):");
+            int[] array1 = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            Console.WriteLine("Enter the elements of the second sorted array (space-separated):");
+            int[] array2 = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            int[] mergedArray = new int[array1.Length + array2.Length];
+
+            int index1 = 0, index2 = 0, indexMerged = 0;
+
+            while (index1 < array1.Length && index2 < array2.Length)
+            {
+                if (array1[index1] <= array2[index2])
+                {
+                    mergedArray[indexMerged++] = array1[index1++];
+                }
+                else
+                {
+                    mergedArray[indexMerged++] = array2[index2++];
+                }
+            }
+
+            while (index1 < array1.Length)
+            {
+                mergedArray[indexMerged++] = array1[index1++];
+            }
+
+            while (index2 < array2.Length)
+            {
+                mergedArray[indexMerged++] = array2[index2++];
+            }
+
+            Console.WriteLine("Merged array in ascending order:");
+            foreach (int item in mergedArray)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            #endregion
+
+            #region Frequency of Each Element in Array
+            Console.WriteLine("Enter the elements of the array (space-separated):");
+            int[] freqArray = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            Dictionary<int, int> frequency = new Dictionary<int, int>();
+
+            foreach (int item in freqArray)
+            {
+                if (frequency.ContainsKey(item))
+                {
+                    frequency[item]++;
+                }
+                else
+                {
+                    frequency[item] = 1;
+                }
+            }
+
+            Console.WriteLine("Frequency of each element:");
+            foreach (var kvp in frequency)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+            }
+            #endregion
+
+            #region Maximum and Minimum Element in Array
+            Console.WriteLine("Enter the elements of the array (space-separated):");
+            int[] maxMinArray = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            int maxElement = maxMinArray[0];
+            int minElement = maxMinArray[0];
+
+            for (int i = 1; i < maxMinArray.Length; i++)
+            {
+                if (maxMinArray[i] > maxElement)
+                {
+                    maxElement = maxMinArray[i];
+                }
+                if (maxMinArray[i] < minElement)
+                {
+                    minElement = maxMinArray[i];
+                }
+            }
+
+            Console.WriteLine($"Maximum element: {maxElement}");
+            Console.WriteLine($"Minimum element: {minElement}");
+            #endregion
+
+            #region Second Largest Element in Array
+            Console.WriteLine("Enter the elements of the array (space-separated):");
+            int[] secLargestArray = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            int first = int.MinValue, second = int.MinValue;
+
+            foreach (int item in secLargestArray)
+            {
+                if (item > first)
+                {
+                    second = first;
+                    first = item;
+                }
+                else if (item > second && item != first)
+                {
+                    second = item;
+                }
+            }
+
+            if (second == int.MinValue)
+            {
+                Console.WriteLine("No second largest element.");
+            }
+            else
+            {
+                Console.WriteLine($"Second largest element: {second}");
+            }
+            #endregion
+
+            #region Longest Distance Between Two Equal Cells
+            Console.WriteLine("Enter the elements of the array (space-separated):");
+            int[] inputArray = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            Dictionary<int, int> firstIndex = new Dictionary<int, int>();
+            int longestDistance = 0;
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                if (firstIndex.ContainsKey(inputArray[i]))
+                {
+                    int distance = i - firstIndex[inputArray[i]];
+                    if (distance > longestDistance)
+                    {
+                        longestDistance = distance;
+                    }
+                }
+                else
+                {
+                    firstIndex[inputArray[i]] = i;
+                }
+            }
+
+            Console.WriteLine($"The longest distance between two equal cells is: {longestDistance}");
+            #endregion
+
+            #region Reverse the Order of Words
+            Console.WriteLine("Enter a list of space-separated words:");
+            string inputString = Console.ReadLine();
+            string[] words = inputString.Split(' ');
+            Array.Reverse(words);
+            Console.WriteLine(string.Join(" ", words));
+            #endregion
+
+            #region Copy Multidimensional Array
+            Console.Write("Enter the number of rows: ");
+            int rows = int.Parse(Console.ReadLine());
+            Console.Write("Enter the number of columns: ");
+            int cols = int.Parse(Console.ReadLine());
+
+            int[,] firstArray = new int[rows, cols];
+            Console.WriteLine("Enter the elements of the first array:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write($"Element [{i},{j}]: ");
+                    firstArray[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            int[,] secondArray = new int[rows, cols];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    secondArray[i, j] = firstArray[i, j];
+                }
+            }
+
+            Console.WriteLine("Elements of the second array:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write(secondArray[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            #endregion
+
+            #region Reverse One Dimensional Array
+            Console.WriteLine("Enter the elements of the array (space-separated):");
+            int[] array = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+            Array.Reverse(array);
+
+            Console.WriteLine("Array in reverse order:");
+            foreach (int item in array)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            #endregion
+
+
+        }
+    }
+}
